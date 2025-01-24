e40e5de7b995a9fb64b957a15e2b3e2f9d0b3b51
developernagor <developernagor@gmail.com>
2025-01-24 13:39:35 +0600

    Update code for deployment

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,10 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-7xl mx-auto`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar></Navbar>
+        <div className="min-h-screen">
         {children}
+        </div>
         <Footer></Footer>
       </body>
     </html>
